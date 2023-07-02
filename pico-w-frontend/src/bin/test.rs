c74586545489fd2413a8837aaae7076816e20ea8
Blake Loring <blake@parsed.uk>
2023-07-02 12:04:00 +0800

    Implement device buttons

--- pico-w-frontend/src/bin/test.rs
+++ pico-w-frontend/src/bin/test.rs
@@ -1,16 +1,19 @@
 #![no_std]
 #![no_main]
 
+use crate::hal::gpio::bank0::{Gpio2, Gpio3, Gpio4, Gpio5, Gpio6, Gpio7};
 use cortex_m_rt::entry;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::{InputPin, OutputPin, StatefulOutputPin};
+use embedded_alloc::Heap;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle, MonoTextStyleBuilder},
+    pixelcolor::{BinaryColor, PixelColor},
+    prelude::*,
+    text::{Baseline, Text},
+};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 use embedded_time::fixed_point::FixedPoint;
 use embedded_time::rate::Extensions;
-use panic_probe as _;
-use rp2040_hal as hal;
-
-use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
-
 use hal::{
     clocks::{init_clocks_and_plls, Clock},
     i2c::I2C,
@@ -18,13 +21,15 @@ use hal::{
     watchdog::Watchdog,
     Sio,
 };
+use panic_probe as _;
+use rp2040_hal as hal;
+use rp2040_hal::gpio::Pin;
+use rp2040_hal::gpio::PullDownInput;
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
+use tetris_core::tetris::{KeyState, Tetris};
 
-use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyle, MonoTextStyleBuilder},
-    pixelcolor::{BinaryColor, PixelColor},
-    prelude::*,
-    text::{Baseline, Text},
-};
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
 
 #[link_section = ".boot2"]
 #[used]
@@ -69,8 +74,54 @@ impl<'a, DI: WriteOnlyDataCommand, SIZE: ssd1306::prelude::DisplaySize>
     }
 }
 
+struct Buttons {
+    pub up: Pin<Gpio2, PullDownInput>,
+    pub left: Pin<Gpio3, PullDownInput>,
+    pub down: Pin<Gpio4, PullDownInput>,
+    pub right: Pin<Gpio5, PullDownInput>,
+    pub a: Pin<Gpio6, PullDownInput>,
+    pub b: Pin<Gpio7, PullDownInput>,
+}
+
+impl Buttons {
+    pub fn a_pressed(&self) -> bool {
+        self.a.is_high().unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub fn b_pressed(&self) -> bool {
+        self.b.is_high().unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub fn up_pressed(&self) -> bool {
+        self.up.is_high().unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub fn down_pressed(&self) -> bool {
+        self.down.is_high().unwrap()
+    }
+
+    pub fn left_pressed(&self) -> bool {
+        self.left.is_high().unwrap()
+    }
+
+    pub fn right_pressed(&self) -> bool {
+        self.right.is_high().unwrap()
+    }
+}
+
 #[entry]
 fn main() -> ! {
+    //Allocator
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
@@ -97,10 +148,17 @@ fn main() -> ! {
     );
     let mut led_pin = pins.gpio25.into_push_pull_output();
 
-    let mut button_one_poll = pins.gpio19.into_pull_down_input();
-    let mut button_one_pwr = pins.gpio20.into_push_pull_output();
+    let mut btn_pwr = pins.gpio0.into_push_pull_output();
+    btn_pwr.set_high().unwrap();
 
-    button_one_pwr.set_high().unwrap();
+    let buttons = Buttons {
+        up: pins.gpio2.into_pull_down_input(),
+        left: pins.gpio3.into_pull_down_input(),
+        down: pins.gpio4.into_pull_down_input(),
+        right: pins.gpio5.into_pull_down_input(),
+        a: pins.gpio6.into_pull_down_input(),
+        b: pins.gpio7.into_pull_down_input(),
+    };
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
@@ -110,11 +168,11 @@ fn main() -> ! {
         pins.gpio11.into_mode(), // I2C 1 SCL
         400_u32.kHz(),
         &mut pac.RESETS,
-        125_000_000_u32.Hz(),
+        clocks.system_clock.freq(),
     );
 
     let interface = I2CDisplayInterface::new(i2c);
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate180)
         .into_buffered_graphics_mode();
 
     led_pin.set_high().unwrap();
@@ -135,33 +193,42 @@ fn main() -> ! {
         text_style,
     };
 
-    let mut current = Point::new(0, 16);
     let mut last_led = false;
 
-    loop {
-        screen.clear();
-        screen.draw_row(0);
-        screen.draw_row(screen.dim.height - 1);
+    let mut tetris = Tetris::new();
 
-        if button_one_poll.is_high().unwrap() {
-            screen.text("Goodbye World", current);
-        } else {
-            screen.text("Hello World", current);
+    loop {
+        let key_state = KeyState {
+            left: buttons.left_pressed(),
+            right: buttons.right_pressed(),
+            rotate: buttons.a_pressed(),
+        };
+        tetris.set_key_state(&key_state);
+        tetris.update();
+
+        screen.clear();
+
+        match tetris {
+            Tetris::Running(ref state) => {
+                state.draw_game_grid(
+                    |x, y, v| {
+                        screen.display.set_pixel(x as u32, y as u32, v);
+                    },
+                    (2, 1),
+                    (2, 2),
+                );
+            }
+            Tetris::Finished => tetris = Tetris::new(),
         }
-        screen.flush();
 
         if last_led {
-            led_pin.set_high().unwrap();
-        } else {
             led_pin.set_low().unwrap();
-        }
-
-        current.x += 1;
-
-        if current.x > 160 {
-            current.x = -60;
+        } else {
+            led_pin.set_high().unwrap();
         }
         last_led = !last_led;
-        delay.delay_ms(50);
+
+        screen.flush();
+        delay.delay_ms(500);
     }
 }
